19a38b7c7e727911361a79285f30bce896bda770
Jared King <j@jaredtking.com>
2015-02-12 19:28:39 -0600

    basic sinatra app

---
+++ webhook.rb
@@ -0,0 +1,22 @@
+require 'json'
+require 'sinatra'
+use Rack::Logger
+require 'stripe_event'
+
+Stripe.api_key = ENV['STRIPE_API_KEY']
+
+StripeEvent.subscribe 'invoice.created' do |event|
+    request.logger.info(event)
+
+    invoice = event.data.object
+    if invoice.amount_due < 0
+        return
+
+    date = Time.at(event.data.object.date).strftime("%b %-d, %Y")
+end
+
+post '/_billing_events' do
+    data = JSON.parse(request.body.read, symbolize_names: true)
+    StripeEvent.instrument(data)
+    200
+end
