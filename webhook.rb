73294a25fe0b85a0e73dce7734bb2af44c958e7f
Jared King <j@jaredtking.com>
2015-02-12 20:31:38 -0600

    attempt to get invoice

--- webhook.rb
+++ webhook.rb
@@ -1,36 +1,30 @@
 require 'json'
 require 'sinatra'
 require 'stripe_event'
-require 'logger'
-
-enable :logging
+require 'net/http'
+require 'uri'
 
 Stripe.api_key = ENV['STRIPE_API_KEY']
 
-class InvoiceCreated
-  def call(event)
+def getInvoice(url)
+    Net::HTTP.post_form(URI.parse(url), {
+        "from" => "Your Name",
+        "to" => "To"
+    })
+end
+
+StripeEvent.subscribe 'invoice.created' do |event|
+    puts event
+
     invoice = event.data.object
     if invoice.amount_due < 0
         return
     end
 
     date = Time.at(event.data.object.date).strftime("%b %-d, %Y")
-  end
-end
 
-class BillingEventLogger
-  def initialize(logger)
-    @logger = logger
-  end
-
-  def call(event)
-    @logger.info "BILLING:#{event.type}:#{event.id}"
-  end
-end
-
-StripeEvent.configure do |events|
-    events.all BillingEventLogger.new(ENV['rack.logger'])
-    events.subscribe 'invoice.created', InvoiceCreated.new
+    pdf = getInvoice('https://invoice-generator.com')
+    puts pdf
 end
 
 post '/_billing_events' do
