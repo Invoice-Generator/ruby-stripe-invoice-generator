c4facaf5941f900504cbbb0164f45c41dfda4ee9
Jared King <j@jaredtking.com>
2015-02-12 22:48:34 -0600

    add logo

--- webhook.rb
+++ webhook.rb
@@ -8,7 +8,14 @@ require 'pony'
 Stripe.api_key = ENV['STRIPE_API_KEY']
 
 def genInvoice(customer, invoice)
-    date = Time.at(invoice.date).strftime("%b %-d, %Y")
+    uri = URI('https://invoice-generator.com')
+
+    from = "Invoiced
+701 Brazos St
+Austin, TX 78748"
+
+    # TODO could get address from metadata
+    to = customer.description
 
     items = invoice.lines.data.map do |line|
         name = line.type == 'subscription' ? line.plan.name : line.description
@@ -21,22 +28,19 @@ def genInvoice(customer, invoice)
 
     # TODO discounts
 
-    uri = URI('https://invoice-generator.com')
-
     params = {
-        "from" => "*Your Company*
-Address
-City, State Zip",
-        "to" => customer.description, # TODO could get address from metadata
+        "logo" => "https://invoiced.com/img/logo-square.png",
+        "from" => from,
+        "to" => to,
         "currency" => invoice.currency,
         "number" => invoice.id,
         "payment_terms" => "Auto-Billed - Do Not Pay",
-        "tax" => invoice.tax_percent,
+        "date" => Time.at(invoice.date).strftime("%b %-d, %Y"),
+        "items" => items,
         "fields" => {
             "tax" => invoice.tax_percent ? "%" : false
         },
-        "date" => date,
-        "items" => items,
+        "tax" => invoice.tax_percent,
         "notes" => "Thanks for being an awesome customer!",
         "terms" => "No need to submit payment. You will be auto-billed for this invoice."
     }
@@ -52,14 +56,6 @@ City, State Zip",
     end
 end
 
-def getBody(customer)
-    "Hi " + customer.description + ",
-
-A new invoice was created on your account as part of your subscription. Please keep the attached invoice for your records. Have a nice day!
-
-- *Your Company*"
-end
-
 StripeEvent.subscribe 'invoice.created' do |event|
     invoice = event.data.object
     puts invoice
@@ -72,11 +68,17 @@ StripeEvent.subscribe 'invoice.created' do |event|
     puts customer
 
     # send the invoice
+    body = "Hi " + customer.description + ",
+
+A new invoice was created on your account as part of your subscription. Please keep the attached invoice for your records. Have a nice day!
+
+- Invoiced"
+
     Pony.mail({
         :to => customer.email,
         :from => 'yourcompany@example.com',
-        :subject => 'Invoice from *Your Company*',
-        :body => getBody(customer),
+        :subject => 'Invoice from Invoiced'
+        :body => body,
         :attachments => {
             "invoice.pdf" => pdf.body,
         },
