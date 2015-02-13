4a443f5b900c8684f4ddd6d03119b34e67629338
Jared King <j@jaredtking.com>
2015-02-12 20:05:32 -0600

    logger

--- webhook.rb
+++ webhook.rb
@@ -1,19 +1,36 @@
 require 'json'
 require 'sinatra'
-use Rack::Logger
 require 'stripe_event'
+require 'logger'
+
+enable :logging
 
 Stripe.api_key = ENV['STRIPE_API_KEY']
 
-StripeEvent.subscribe 'invoice.created' do |event|
-    logger.info(event)
-
+class InvoiceCreated
+  def call(event)
     invoice = event.data.object
     if invoice.amount_due < 0
         return
     end
 
     date = Time.at(event.data.object.date).strftime("%b %-d, %Y")
+  end
+end
+
+class BillingEventLogger
+  def initialize(logger)
+    @logger = logger
+  end
+
+  def call(event)
+    @logger.info "BILLING:#{event.type}:#{event.id}"
+  end
+end
+
+StripeEvent.configure do |events|
+    events.all BillingEventLogger.new(logger)
+    events.subscribe 'invoice.created', InvoiceCreated.new
 end
 
 post '/_billing_events' do
