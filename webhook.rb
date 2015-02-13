4d389286f844dc55c3671d2d8362366d5c4d99e7
Jared King <j@jaredtking.com>
2015-02-12 21:13:11 -0600

    progress on invoice

--- webhook.rb
+++ webhook.rb
@@ -7,10 +7,18 @@ require 'pony'
 
 Stripe.api_key = ENV['STRIPE_API_KEY']
 
-def getInvoice(url)
-    Net::HTTP.post_form(URI.parse(url), {
+def getInvoice(customer, invoice)
+    date = Time.at(invoice.date).strftime("%b %-d, %Y")
+
+    Net::HTTP.post_form(URI.parse("https://invoice-generator.com"), {
         "from" => "*Your Company*",
-        "to" => "To"
+        "to" => customer.name,
+        "number" => invoice.id,
+        "payment_terms" => "Auto-Billed - Do Not Pay",
+        "date" => date,
+        # TODO items
+        "notes" => "Thanks for being an awesome customer!",
+        "terms" => "No need to submit payment. You will be auto-billed for this invoice."
     })
 end
 
@@ -22,18 +30,23 @@ StripeEvent.subscribe 'invoice.created' do |event|
         return
     end
 
-    date = Time.at(event.data.object.date).strftime("%b %-d, %Y")
-
     # get customer from stripe
     customer = Stripe::Customer.retrieve(invoice.customer)
     puts customer
 
     # fetch invoice pdf
-    pdf = getInvoice('https://invoice-generator.com')
+    pdf = getInvoice(customer)
+
+    # send the invoice
     Pony.mail({
         :to => customer.email,
         :from => 'yourcompany@example.com',
         :subject => 'Invoice from *Your Company*',
+        :body => "Hi #{customer.name},
+
+A new invoice was created on your account as part of your subscription. Please keep the attached invoice for your records. Have a nice day!
+
+- *Your Company*",
         :attachments => {
             "invoice.pdf" => pdf.body
         }
